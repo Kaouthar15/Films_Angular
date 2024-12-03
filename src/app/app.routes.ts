41b5468c50313d61441007d6ac5415945e3d86cb
Anas BenOmar <138506861+AnsBenO@users.noreply.github.com>
2024-12-03 02:48:34 +0100

    refactored + configured tailwind and fontawesome + configured routing + created navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,47 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    title: 'CinemaFlow - Home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'actors',
+    title: 'CinemaFlow - Actors',
+    loadComponent: () =>
+      import('./pages/actor/actors/actors.component').then(
+        (m) => m.ActorsComponent
+      ),
+  },
+  {
+    path: 'directors',
+    title: 'CinemaFlow - Directors',
+    loadComponent: () =>
+      import('./pages/director/directors/directors.component').then(
+        (m) => m.DirectorsComponent
+      ),
+  },
+  {
+    path: 'films',
+    title: 'CinemaFlow - Films',
+    loadComponent: () =>
+      import('./pages/film/films/films.component').then(
+        (m) => m.FilmsComponent
+      ),
+  },
+  {
+    path: 'ratings',
+    title: 'CinemaFlow - Ratings',
+    loadComponent: () =>
+      import('./pages/rating/ratings/ratings.component').then(
+        (m) => m.RatingsComponent
+      ),
+  },
+];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,81 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  faBars,
+  faCartShopping,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import {
+  Component,
+  DestroyRef,
+  HostListener,
+  inject,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  destroy = inject(DestroyRef);
+  router = inject(Router);
 
+  showCart: WritableSignal<boolean> = signal(false);
+  showInbox: WritableSignal<boolean> = signal(false);
+  isMenuOpen: WritableSignal<boolean> = signal(false);
+
+  menuIcon = faBars;
+  xMarkIcon = faXmark;
+  cartIcon = faCartShopping;
+
+  toggleMenu() {
+    this.isMenuOpen.set(!this.isMenuOpen());
+  }
+
+  toggleMenuOutsideClick($event: MouseEvent) {
+    const menuDiv = document.querySelector('#menuDiv');
+    const targetElement = $event.target as HTMLElement;
+    if (!menuDiv?.contains(targetElement)) {
+      this.toggleMenu();
+    }
+  }
+
+  openCart() {
+    this.showCart.set(true);
+  }
+
+  closeCart() {
+    this.showCart.set(false);
+  }
+  openInbox() {
+    this.showInbox.set(true);
+  }
+
+  closeInbox() {
+    this.showInbox.set(false);
+  }
+
+  logout() {
+    // this.authService
+    //   .logout()
+    //   .pipe(takeUntilDestroyed(this.destroy))
+    //   .subscribe((response) => {
+    //     this.router.navigate(['/']);
+    //     this.notificationStore.notify(
+    //       response.message,
+    //       NotificationType.SUCCESS
+    //     );
+    //   });
+  }
+
+  @HostListener('document:scroll')
+  handleScrolling() {
+    this.isMenuOpen.set(false);
+  }
 }
